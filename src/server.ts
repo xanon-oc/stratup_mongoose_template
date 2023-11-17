b855cf8dd99154617e3328291cf42b27e5cead4f
Niloy Roy <niloyroy184@gmail.com>
2023-11-17 19:11:34 +0600

    starter file

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app';
+import config from './config';
+
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
